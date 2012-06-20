17b3a60630b55c34cc57159ecb0d661d2789ec12
Ray <rayshobbyshop@gmail.com>
2012-06-19 20:27:01 -0400

    minor change to home.js

--- scripts/java/svc1.6/home.js
+++ scripts/java/svc1.6/home.js
@@ -11,7 +11,7 @@ w("<h4><a href=/>[Refresh]</a>&nbsp;&nbsp;<a href=/vo>[Options]</a>&nbsp;&nbsp;<
 // print device information
 w("<b>Firmware version</b>: "+(ver/10>>0)+"."+(ver%10)+"<br>");
 w("<b>Device time</b>: "+datestr(devt*1000));
-w("<script type=\"text/javascript\" src=\"http://rayshobby.net/scripts/java/svc1.6/"+(((typeof mm!="undefined")&&mm)?"manualmode.js":"progmode.js")+"\"></script>");
+w("<script type=\"text/javascript\" src=\"http://rayshobby.net/scripts/java/svc1.6/"+((mm)?"manualmode.js":"progmode.js")+"\"></script>");
 // print other information
 w("</span><br><b>Operation</b>: "+(en?("enabled").fontcolor("green"):("DISABLED").fontcolor("red")));
 w("<br><b>Raindelay</b>: "+(rd?("ON").fontcolor("red")+" (till "+datestr(rdst*1000)+")":("off").fontcolor("black")));
@@ -25,8 +25,6 @@ w("<hr>");
 w("<form name=hf action=cv method=get><p>Password:<input type=password size=10 name=pw></p>");
 w("<input type=hidden name=en><input type=hidden name=rd value=0><input type=hidden name=rst value=0><input type=hidden name=mm value=0></form>");
 w("<button style=\"height:36\" onclick=\"hf.elements[1].value="+(1-en)+";hf.submit();\">"+imgstr(en?"stop":"start")+(en?"Stop Operation":"Start Operation")+"</button>");
-if(typeof mm!="undefined") {
 w("<button style=\"height:36\" onclick=\"hf.elements[4].value="+(1-mm)+";hf.submit();\">"+imgstr(mm?"auto":"manual")+(mm?"Manual Off":"Switch to Manual")+"</button>");
-}
 w("<button style=\"height:36\" onclick=\"setrd(hf,2)\">"+imgstr("rain")+"Rain Delay</button>");
 w("<button style=\"height:36\" onclick=\"hf.elements[3].value=1;hf.submit();\">"+imgstr("reboot")+"Reboot</button>");
