c5285a801accf28db72c57a460767e16660b4233
Ray <rayshobbyshop@gmail.com>
2013-07-11 07:07:04 -0400

    check in Dan's OSPi interval_program Jul 10 update

---
+++ OpenSprinkler Pi/software/demos/interval_program/OSPi/ospi_addon.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+import ospi
+
+  #### Add any new page urls here ####
+ospi.urls.extend([]) # example: (['/c1', 'custom_page_1, '/c2', 'custom_page_2', '/c3', 'custom_page_3'])
+
+##print ospi.urls # for testing
+
+  #### add new functions and classes here ####
+
+  ### Example custom class. Uncomment and use to create a new web page. ###
+##class custom_page_1:
+##   """Add description here"""
+##   def GET(self):
+##      custpg = '<!DOCTYPE html>\n'
+##      #Insert Custom Code here.
+##      return custpg
+
+
+
+  
